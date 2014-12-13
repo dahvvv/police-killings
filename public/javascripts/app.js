91a12fcb6ad2d0a3d7fd0c2d89125950365e667c
dahvvv <davidjosephkoenig@gmail.com>
2014-12-13 10:41:44 -0500

    map is heatmap

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -29,26 +29,26 @@ function featureToGeoFormat(lat,lon,i){
   return geoFeature;
 };
 
-var geojsonMarkerOptions = {
-    radius: 3,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
 function makeMap(map, geojson){
   var layer = L.mapbox.featureLayer().addTo(map);
   layer.setGeoJSON(geojson);
   map.fitBounds(layer.getBounds());
 };
 
+function makeHeatMap(map,coords){
+  var heatLayer = L.heatLayer(coords, {
+    // radius: 25,
+    // blur: 0,
+    // gradient: 1,
+    maxZoom: 9,
+  }).addTo(map);
+  // map.fitBounds(heatLayer.getBounds());
+};
+
 var killingList = new KillingList;
 var geoFeatureArr = [];
 var geoJSON;
 var map;
-var markerList = document.getElementById('marker-list');
 
 
 // map.featureLayer.eachLayer(function(layer) {
@@ -64,24 +64,23 @@ $(function(){
 
   var map = L.mapbox.map('map-one', 'examples.map-i86l3621', {
     scrollWheelZoom: false,
-    legendControl: {
-      position: 'topright'
-    }
-  });
-  // heat = L.heatLayer([], { maxZoom: 12 }).addTo(map);
+  }).setView([39.8282,-98.5795],4),
+  heat = L.heatLayer([]);
 
   killingList.fetch({
     reset: true,
     success: function(data){
+      var coordsArr = [];
       data.toJSON().forEach(function(elem, i){
-        console.log(elem.lat);
         var lat = elem.lat;
         var lon = elem.lng;
-        var geoFeature = featureToGeoFormat(lat,lon,i);
-        geoFeatureArr.push(geoFeature);
+        // var geoFeature = featureToGeoFormat(lat,lon,i);
+        // geoFeatureArr.push(geoFeature);
+        coordsArr.push([lat,lon])
       });
-      var geoJSON = geoJSONify(geoFeatureArr);
-      makeMap(map, geoJSON);
+      // var geoJSON = geoJSONify(geoFeatureArr);
+      // makeMap(map, geoJSON);
+      makeHeatMap(map, coordsArr);
     }
   });
 });
